4f44c094f597893492ff1d957da3c77a2fe1c790
ikerUrle <ikerurlezaga@gmail.com>
2020-09-06 16:39:32 +0200

    Updates

---
+++ precache-manifest.6f99e650c50b1aa3b7e51fddcf512b17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b153491bdd792c7de84a6e137b879270",
+    "url": "/random/index.html"
+  },
+  {
+    "revision": "8a9886343cece1b865d9",
+    "url": "/random/static/css/main.27ff6dcf.chunk.css"
+  },
+  {
+    "revision": "563bb1c71b8be5e7a956",
+    "url": "/random/static/js/2.f8cf1aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random/static/js/2.f8cf1aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9886343cece1b865d9",
+    "url": "/random/static/js/main.79875695.chunk.js"
+  },
+  {
+    "revision": "c0b323413d1e0ab40063",
+    "url": "/random/static/js/runtime-main.745496f5.js"
+  }
+]);
